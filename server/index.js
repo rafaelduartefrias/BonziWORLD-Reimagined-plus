19db3d9515071548aac15aba2f6808f92cff44e7
thebeatles423 <144031272+thebeatles423@users.noreply.github.com>
2023-09-04 15:21:37 +0100

    optimization(tm)

--- server/index.js
+++ server/index.js
@@ -1,89 +1,68 @@
-// ========================================================================
 // Server init
-// ========================================================================
-
-// Filesystem reading functions
 const fs = require('fs-extra');
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const sanitize = require('sanitize-html');
+const Log = require('./log.js');
+const Ban = require('./ban.js');
+const Utils = require('./utils.js'); // Include utils.js
+const Meat = require('./meat.js');
+const Console = require('./console.js');
 
-// Load settings
+// Load settings or create a new settings file if it doesn't exist
+let settings;
 try {
-	stats = fs.lstatSync('settings.json');
+  fs.statSync('settings.json');
+  settings = require('./settings.json');
 } catch (e) {
-	// If settings do not yet exist
-	if (e.code == "ENOENT") {
-		try {
-			fs.copySync(
-				'settings.example.json',
-				'settings.json'
-			);
-			console.log("Created new settings file.");
-		} catch(e) {
-			console.log(e);
-			throw "Could not create new settings file.";
-		}
-	// Else, there was a misc error (permissions?)
-	} else {
-		console.log(e);
-		throw "Could not read 'settings.json'.";
-	}
+  if (e.code === 'ENOENT') {
+    try {
+      fs.copySync('settings.example.json', 'settings.json');
+      console.log('Created new settings file.');
+      settings = require('./settings.json');
+    } catch (e) {
+      console.log(e);
+      throw "Could not create new settings file.";
+    }
+  } else {
+    console.log(e);
+    throw "Could not read 'settings.json'.";
+  }
 }
 
-// Load settings into memory
-const settings = require("./settings.json");
-
 // Setup basic express server
-var express = require('express');
-var app = express();
-if (settings.express.serveStatic)
-	app.use(express.static('../build/www'));
-var server = require('http').createServer(app);
+const app = express();
+if (settings.express.serveStatic) {
+  app.use(express.static('../build/www'));
+}
+const server = http.createServer(app);
 
 // Init socket.io
-var io = require('socket.io')(server);
-var port = process.env.PORT || settings.port;
-
+const io = socketIO(server);
+const port = process.env.PORT || settings.port;
 exports.io = io;
 
-// Init sanitize-html
-var sanitize = require('sanitize-html');
-
-// Init winston loggers (hi there)
-const Log = require('./log.js');
+// Init winston loggers
 Log.init();
 const log = Log.log;
 
 // Load ban list
-const Ban = require('./ban.js');
 Ban.init();
 
 // Start actually listening
-server.listen(port, function () {
-	console.log(
-		" Welcome to BonziWORLD!\n",
-		"Time to meme!\n",
-		"----------------------\n",
-		"Server listening at port " + port
-	);
+server.listen(port, () => {
+  console.log(
+    " Welcome to BonziWORLD!\n",
+    "Time to meme!\n",
+    "----------------------\n",
+    "Server listening at port " + port
+  );
 });
 app.use(express.static(__dirname + '/public'));
 
-// ========================================================================
-// Banning functions
-// ========================================================================
-
-// ========================================================================
-// Helper functions
-// ========================================================================
-
-const Utils = require("./utils.js")
-
-// ========================================================================
 // The Beef(TM)
-// ========================================================================
-
-const Meat = require("./meat.js");
 Meat.beat();
 
 // Console commands
-const Console = require('./console.js');
 Console.listen();
